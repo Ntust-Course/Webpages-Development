0fee6e80ca997640d98473eeb08c691459997113
SheiUn <d215039@outlook.com>
2018-03-20 16:15:50 +0800

    Version 1.7.3

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,7 +1,15 @@
 $("#head").on('mouseenter touchstart', function() {
   $(this).attr("src","images/head2.jpg");
-})
+});
 
 $("#head").on('mouseleave touchend', function() {
   $(this).attr("src","images/head.jpg");
+});
+
+$("li > a").on('mouseenter touchstart', function() {
+  $(this).css('color', 'white');
+})
+
+$("li > a").on('mouseleave touchend', function() {
+  $(this).css('color', '#2f2f2f');
 })
