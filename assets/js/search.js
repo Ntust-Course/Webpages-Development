bd919c01932f3938227922a4e030d0c87e24963a
SheiUn <d215039@outlook.com>
2018-03-25 02:20:23 +0800

    test

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,7 +7,7 @@ $("form.search-content").submit(function(e) {
     values[this.name] = $(this).val();
   });
   var key = values['key'];
-  var input = '<h3>你的作業查詢小助手</h3><p>助教不會寫程式 請不要操他<br>有問題請回報助教</p>';
+  var input = '<h3>你的作業查詢小助手</h3><p>如果有問題<br>點擊螢幕右下角<br>向助教揮手</p>';
   var url = 'https://script.google.com/macros/s/AKfycbwoyxrSc5dmqyZa-u2nd3F5FFXjw0_6DhqzTzgybUAe19UmFdU/exec?key=';
   $.getJSON(url + key, function(data) {
     var output = input;
@@ -21,6 +21,7 @@ $("form.search-content").submit(function(e) {
 
     $("div.search-container").children().hide(); 
     $("div.search-container").append("<p>"+ output + "</p>");
+    $("div.search-container").append('<p>如果你喜歡我們製作的網頁<br>歡迎點擊 $ 圖示捐贈我們醫療費<br>美金 1 元以上的任何價格<br>都可能是治好我們不治之症的關鍵</p>');
     $("div.loader").hide();
   }).fail(function() {
     console.log('error id not found')
